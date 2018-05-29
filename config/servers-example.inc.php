e229fd3a34bbdf7e434f00df043c25b390b564ef
ymtah <39276103+ymtah@users.noreply.github.com>
2018-05-29 18:53:28 +0100

    wesdf

---
+++ config/servers-example.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+$PokemonServers = array (
+  'showdown' => 
+  array (
+    'name' => 'GOBF',
+    'id' => 'GOBF',
+    'server' => 'http://108.248.181.14-413.psim.us/',
+    'port' => 413,
+  ),
+);
